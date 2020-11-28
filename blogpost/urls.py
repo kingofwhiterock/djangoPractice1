cf6c6792541a14bf4f0405e17568a80c685a5120
Apoi <49585634+kingofwhiterock@users.noreply.github.com>
2020-11-29 04:59:05 +0900

    the end of 4-5

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.urls import path
+from .views import BlogListView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('list/', BlogListView.as_view()),
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import BlogModel
+
 
 # Create your views here.
+class BlogListView(ListView):
+    template_name = 'list.html'
+    model = BlogModel
